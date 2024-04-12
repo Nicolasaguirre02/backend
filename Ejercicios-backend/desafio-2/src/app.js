fb4f9ab83c26d4be6100e46a5ff99f313b1412dd
nicolas aguirre <nico.aguirre02@hotmail.com>
2024-04-11 23:17:58 -0300

    Corecciones desafio-2

--- Ejercicios-backend/desafio-2/src/app.js
+++ Ejercicios-backend/desafio-2/src/app.js
@@ -2,17 +2,14 @@ const fs = require('fs').promises;
 
 class ProductManager{
 
-    constructor(path, nombrearchvio){
+    constructor(path){
         this.path = path
-        this.nombrearchivo = nombrearchvio
-        
-        this.archivo = path+nombrearchvio
     }
 
 
-    async crearArchivo(){
+    async newFile(){
         try {
-           return await fs.writeFile(`${this.path}/${this.nombrearchivo}`,'');
+           return await fs.writeFile(`${this.path}`,'');
         } catch (error) {
             console.log("Error al crerar el archivo :", error)
         }
@@ -20,15 +17,16 @@ class ProductManager{
 
     async getProducts(){
         try {
-            const data = await fs.readFile(this.archivo, 'utf8');
-            if(!data){
-                return []
-            }else{
-                return JSON.parse(data)
-            }
+            const data = await fs.readFile(this.path, { encoding: 'utf8' });
+
+             if (!data || data.trim() === "") {
+                return [];
+            } else {
+                return JSON.parse(data);
+            } 
             
         } catch (error) {
-             console.error('Error al leer el archivo:'); // Maneja el error imprimiéndolo en la consola
+             console.error('Error al leer el archivo:');
         }
     }
 
@@ -36,19 +34,60 @@ class ProductManager{
 
     async addProduct(productos){
         try {
-            fs.writeFile(this.archivo, JSON.stringify(productos, null, 2))  
+            const listaProductos = await this.getProducts();
+            const newId = listaProductos.length + 1;
+            const newProduct = {...productos, id:newId}
+            listaProductos.push(newProduct) 
+            await fs.writeFile(this.path, JSON.stringify(listaProductos, null, 2))  
         } catch (error) {
-            console.log("Error al guardar producto en el acrhivo")
+            console.log("Error al guardar el producto en el acrhivo")
         }
     }
 
-  
+
+    async getProductById(idProducto){
+        try {
+            const listaProductos = await this.getProducts();
+            const productoEncontrado = listaProductos.find((producto) => producto.id = idProducto);
+            console.log("Producto con ID " ,idProducto, " encontrado: ", productoEncontrado)
+
+        } catch (error) {
+            console.error(`No existe producto con id: ${idProducto}`)
+        }
+    }
+
     
+    async updateProduct(idProducto){
+        try {
+            const listaProductos = await this.getProducts();
+            const productoModificar =  listaProductos.find(producto => producto.id === idProducto);
+            productoModificar.price = 900;
+            productoModificar.stock = 18;
+            await fs.writeFile(this.path, JSON.stringify(listaProductos, null, 2))  
+            console.log("Producto modificado")
+        } catch (error) {
+            console.log("El producto que quieres modificar no existe en la lista")
+        }
+    }
+
+
+    async deleteProduct(idProducto){
+        try {
+            const lista_productos = await this.getProducts();
+            const nuevaListaProductos = [];
+            lista_productos.find((producto) => {
+                if(producto.id != idProducto){
+                    nuevaListaProductos.push(producto)
+                }
+            })
+            await fs.writeFile(this.path, JSON.stringify(nuevaListaProductos, null, 2))  
+            console.log("Producto eliminado correctamente")
     
+        } catch (error) {
+            console.log("Error al eliminar el producto")
+        }
+    }
 
 }
-
-/* const hola = new ProductManager('../','productos.json')
- hola.crearArchivo() */ /* Esta funcion se debe ejecutar una sola vez, ya que crea el archivo  */
 
 module.exports = ProductManager
--- Ejercicios-backend/desafio-2/src/consumir_productos.js
+++ Ejercicios-backend/desafio-2/src/consumir_productos.js
@@ -1,109 +1,45 @@
 const ProdcutManage = require('./app.js');
 
-const productos = new ProdcutManage('./','productos.json');
+const productos = new ProdcutManage('./productos.json');
 
-//Crear un nuevo archvio
-async function crearNuevoArchvio(){
-    await productos.crearArchivo()
+//Crear un nuevo archivo
+async function createFile(){
+    await productos.newFile()
 }
-crearNuevoArchvio();  //<--- ESTE METODO SOLO SE EJECUTA UNA VEZ YA QUE CREA EL ARCHIVO
+createFile();    //<--- ESTE METODO SOLO SE EJECUTA UNA VEZ YA QUE CREA EL ARCHIVO
 
-//Agregfar un nuevo producto
-async function crearNuevoProducto(title, description, price, thumbnail, code, stock){
-    try {
-        const listaProductos = await productos.getProducts();
-        const id_generar = listaProductos.length
-        const nuevoProducto = {
-            id:id_generar + 1,
-            title:title,
-            description:description,
-            price:price,
-            thumbnail:thumbnail,
-            code:code,
-            stock:stock
-        }
-        listaProductos.push(nuevoProducto);
 
-        productos.addProduct(listaProductos)
-
-        console.log("Producto agregado correctamente"); 
-        
-    } catch (error) {
-        console.log("Error al crear un nuevo producto")
-    }
-}
-crearNuevoProducto("producto prueba1", "este es un producto prueba", 200, "sin imagen", "abc123", 25); 
-crearNuevoProducto("producto prueba1", "este es un producto prueba", 200, "sin imagen", "abc123", 25); 
+//Agregar un nuevo producto
+productos.addProduct({
+    title:"Producto prueba",
+    description:"probando",
+    price:"400",
+    thumbnail:"thumbnail.com",
+    code:"A12v",
+    stock:45
+})   
 
 
 
-async function leerArchvioProductos(){
+async function readFileProducts(){
     try {
         const data = await productos.getProducts();
-        console.log(data)
+        console.log("Listado de productos: ",data)
     } catch (error) {
         console.log("Error al leer el archivo productos.json")
     }
 }
-leerArchvioProductos(); 
-
-
-
-
-
-
+readFileProducts();
 
 
 
 //Buscar producto por ID
-async function getProductById(id){
-    try {
-        const lista_productos = await productos.getProducts();
-        const producto =  lista_productos.find(produc => produc.id == id);
-        console.log("Este es el producto por ID: ",producto)
-    } catch (error) {
-        console.log("Error al buscar un producto por ID")
-    }
-}
-/* getProductById(1); */
-
-
-
+productos.getProductById(4)  
 
 
 //Modificar producto por ID
-async function updateProduct(id){
-    try {
-        const lista_productos = await productos.getProducts();
-        const productoModificar =  lista_productos.find(produc => produc.id == id);
-        productoModificar.price = 900;
-        productoModificar.stock = 18;
-        productos.addProduct(lista_productos);
-        console.log("Producto modificado correctamente")
-    } catch (error) {
-        console.log("Error al modificar producto")
-    }
-}
-/* updateProduct(2); */
-
-
-
+/* productos.updateProduct(1);   */
+ 
 
 //Eliminar producto por ID
-async function deleteProduct(id){
-    try {
-        const lista_productos = await productos.getProducts();
-        const nuevaListaProductos = [];
-        lista_productos.find((produc) => {
-            if(produc.id != id){
-                nuevaListaProductos.push(produc)
-            }
-        })
-        productos.addProduct(nuevaListaProductos)
-        console.log("Producto eliminado correctamente")
-
-    } catch (error) {
-        console.log("Error al eliminar el producto")
-    }
-}
-/* deleteProduct(id); */
+/*  productos.deleteProduct(2);   */
